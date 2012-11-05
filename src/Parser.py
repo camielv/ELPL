d60c35f8e777d0822d4daf46b1d9afc0ed3bdb46
Camiel Verschoor <verschoor@uva.nl>
2012-11-05 22:18:53 +0100

    Init commit

---
+++ src/Parser.py
@@ -0,0 +1,9 @@
+string = "(TOP (S (PP (IN In) (NP (NP (DT an) (NP@ (NNP Oct.) (NP@ (CD 19) (NN review)))) (NP@ (PP (IN of) (NP (`` ``) (NP@ (NP (DT The) (NN Misanthrope)) (NP@ ('' '') (PP (IN at) (NP (NP (NNP Chicago) (POS 's)) (NP@ (NNP Goodman) (NNP Theatre)))))))) (PRN (-LRB- -LRB-) (PRN@ (`` ``) (PRN@ (S (NP (VBN Revitalized) (NNS Classics)) (VP (VBP Take) (VP@ (NP (DT the) (NN Stage)) (PP (IN in) (NP (NNP Windy) (NNP City)))))) (PRN@ (, ,) (PRN@ ('' '') (PRN@ (NP (NN Leisure) (NP@ (CC &) (NNS Arts))) (-RRB- -RRB-)))))))))) (S@ (, ,) (S@ (NP (NP (NP (DT the) (NN role)) (PP (IN of) (NP (NNP Celimene)))) (NP@ (, ,) (NP@ (VP (VBN played) (PP (IN by) (NP (NNP Kim) (NNP Cattrall)))) (, ,)))) (S@ (VP (VBD was) (VP (ADVP (RB mistakenly)) (VP@ (VBN attributed) (PP (TO to) (NP (NNP Christina) (NNP Haag)))))) (. .))))) )"
+
+class Parser():
+    def __init__(self):
+        pass
+
+    def parse(self, parse):
+        pass
+
